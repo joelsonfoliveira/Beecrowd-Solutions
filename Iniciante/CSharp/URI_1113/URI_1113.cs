38a2410e9ac03a14ae9bfcae021340d714b17f2c
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2024-02-16 23:50:44 -0300

    PROBLEMA: 1113 - Crescente e Decrescente

---
+++ Iniciante/CSharp/URI_1113/URI_1113.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+public class URI_1113
+{
+    public static void Main(string[] args)
+    {
+        int x, y;
+        string[] valores = Console.ReadLine().Split(' ');
+
+        x = int.Parse(valores[0], CultureInfo.InvariantCulture);
+        y = int.Parse(valores[1], CultureInfo.InvariantCulture);
+
+        while (x != y)
+        {
+            if (x < y)
+            {
+                Console.WriteLine("Crescente");
+            }
+            else
+            {
+                Console.WriteLine("Decrescente");
+            }
+
+            valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0], CultureInfo.InvariantCulture);
+            y = int.Parse(valores[1], CultureInfo.InvariantCulture);
+        }
+    }
+}
