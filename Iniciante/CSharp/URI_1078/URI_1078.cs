df91a81a4102fb4f5f87965edb8e340f390ae4ec
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2024-02-20 22:26:28 -0300

    URI_1078

---
+++ Iniciante/CSharp/URI_1078/URI_1078.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+public class URI_1078
+{
+    public static void Main(string[] args)
+    {
+        int N;
+
+        N = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = i * N;
+            Console.WriteLine(i + " x " + N + " = " + resultado);
+        }
+    }
+}
